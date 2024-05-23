b3fcf6f75a128dd3b78e0f759698eac76b25656d
Sachin <007mishrasachinmishra@gmail.com>
2024-05-24 01:34:31 +0530

    using a reusable component in the pages

--- app/page.js
+++ app/page.js
@@ -1,32 +1,3 @@
-import { IoMdStats } from "react-icons/io";
-
 export default function Home() {
-  return (
-  <header className="container max-w-2xl px-4 py-4 mx-auto">
-
-    <div className="flex items-center justify-between">
-    {/* User Info - user section */}
-    <div className="flex items-center gap-2">
-      {/* image of usep */}
-      <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
-        <img className="w-full h-full object-cover"
-        src="./assets/profile.jpg" alt="Profile picture" 
-        />
-      </div>
-
-      {/* username */}
-      <small>Hii, sachin</small>
-    </div>
-    {/* right side of Navigaton */}
-     <nav className="flex items-center gap-2">
-        <div>
-          <IoMdStats className="text-2xl"/> 
-        </div>
-        <div>
-          <button className="btn btn-danger">Sign Out</button>
-        </div>
-     </nav>
-   </div>
-  </header>
-  );
+  return <p>Hello Jii</p>   
 }
