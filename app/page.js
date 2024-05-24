35c6587308a72f074c6570410d1754550617fc29
Sachin <007mishrasachinmishra@gmail.com>
2024-05-24 19:09:08 +0530

    created expense section

--- app/page.js
+++ app/page.js
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <p>Hello Jii</p>   
+  return (
+  <main className="container max-w-2xl px-4 mx-auto">
+    <section className="py-3">
+      <small className="text-gray-300 text-md">Available Balance</small>
+
+      <h2 className="text-2xl font-bold text-white">$100 000.00</h2>
+    </section>
+
+    <section className="flex item-center gap-2 py-3">
+      <button  className="btn btn-primary">+ Income</button>
+      <button  className="btn btn-primary-outline">+ Expenses</button>
+    </section>
+
+  </main>
+  );   
 }
