c4ef58ed606da499e559a97f4ad482f3afaae951
Sachin <007mishrasachinmishra@gmail.com>
2024-06-07 16:58:23 +0530

    updated fav icon

--- components/Navigation.js
+++ components/Navigation.js
@@ -9,44 +9,44 @@ function Nav(){
     const {user,loading, Logout} = useContext(authContext);
 
     return (
-      <header className="container max-w-2xl px-6 py-6 mx-auto">
+    <header className="container max-w-2xl px-6 py-6 mx-auto">
 
-    <div className="flex items-center justify-between">
-    {/* User Info - user section */}
-        {user && !loading && (
-            <div className="flex items-center gap-2">
-            {/* image of usep */}
-              <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
-                <img 
-                className="object-cover w-full h-full "
-                src= {user.photoURL} 
-                alt={user.displayName} 
-                referrerPolicy="no-referrer"
-                />
+      <div className="flex items-center justify-between">
+          {/* User Info - user section */}
+          {user && !loading && (
+              <div className="flex items-center gap-2">
+              {/* image of usep */}
+                <div className="h-[40px] w-[40px] rounded-full overflow-hidden">
+                  <img 
+                  className="object-cover w-full h-full "
+                  src= {user.photoURL} 
+                  alt={user.displayName} 
+                  referrerPolicy="no-referrer"
+                  />
+                </div>
+              {/* username */}
+                <small>Hii, {user.displayName}!</small>
               </div>
-            {/* username */}
-              <small>Hii, {user.displayName}!</small>
-            </div>
-        )}
-    
-    {/* right side of Navigaton */}
-        {user && !loading && (
-            <nav className="flex items-center gap-4">
-              <div>
-                <a href="#stats">
-                  <IoMdStats className="text-3xl"/> 
-                </a>
-              </div>
-              <div>
-                <button 
-                onClick={Logout}
-                className="btn btn-danger">Sign Out</button>
-              </div>
-            </nav>
-        )}
-     
-       </div>
-      </header>
+          )}
+      
+          {/* right side of Navigaton */}
+          {user && !loading && (
+              <nav className="flex items-center gap-4">
+                <div>
+                  <a href="#stats">
+                    <IoMdStats className="text-3xl"/> 
+                  </a>
+                </div>
+                <div>
+                  <button 
+                  onClick={Logout}
+                  className="btn btn-danger">Sign Out</button>
+                </div>
+              </nav>
+          )}
+      
+      </div>
+    </header>
     );
 }
 export default Nav;
